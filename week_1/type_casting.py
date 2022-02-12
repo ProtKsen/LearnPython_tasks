720304bc56d69a56ede4fa6f226ef7e67b03a55f
ProtKsen <protcenko.kseniya@gmail.com>
2022-02-12 22:26:03 +0500

    add task type casting

---
+++ week_1/type_casting.py
@@ -0,0 +1,8 @@
+"""
+Выведите на экран при помощи print() результат работы
+"""
+print(float('1'))   # 1.0
+print(int('2.5'))   # ValueError
+print(bool(1))      # True
+print(bool(''))     # False
+print(bool(0))      # False
