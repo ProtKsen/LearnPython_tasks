17fa853cd242e65c90c1a9fa20768a144872a62d
ProtKsen <protcenko.kseniya@gmail.com>
2022-02-13 10:07:02 +0500

    add task lists

---
+++ week_1/lists.py
@@ -0,0 +1,10 @@
+my_list = [3, 5, 7, 9, 10.5]
+print('Список', my_list)
+my_list.append('Python')
+print("Обновленный список", my_list)
+print("Длина списка", len(my_list))
+print("Первый элемент", my_list[0])
+print("Последний элемент", my_list[-1])
+print("Элементы со 2го по 4ый", my_list[1:4])
+my_list.remove('Python')
+
